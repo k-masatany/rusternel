c46c1e90e72e2824f7eb99784f6d28513997e0c3
k-masatany <masatani@fusic.co.jp>
2017-08-22 23:08:53 +0900

    未使用関数のワーニングが出ないように修正

--- src/x86.rs
+++ src/x86.rs
@@ -4,6 +4,7 @@ pub unsafe fn hlt() {
 }
 
 #[inline(always)]
+#[allow(dead_code)]
 pub unsafe fn inb(port: u16) -> u8 {
     let data: u8;
     asm!("inb %dx,%al" : "={al}" (data) : "{dx}" (port));
@@ -11,6 +12,7 @@ pub unsafe fn inb(port: u16) -> u8 {
 }
 
 #[inline(always)]
+#[allow(dead_code)]
 pub unsafe fn inw(port: u16) -> u16 {
     let data: u16;
     asm!("inw %dx,%ax" : "={ax}" (data) : "{dx}" (port));
@@ -18,6 +20,7 @@ pub unsafe fn inw(port: u16) -> u16 {
 }
 
 #[inline(always)]
+#[allow(dead_code)]
 pub unsafe fn inl(port: u16) -> u32 {
     let data: u32;
     asm!("inl %dx,%eax" : "={eax}" (data) : "{dx}" (port));
@@ -25,16 +28,19 @@ pub unsafe fn inl(port: u16) -> u32 {
 }
 
 #[inline(always)]
+#[allow(dead_code)]
 pub unsafe fn outb(port: u16, val: u8) {
     asm!("outb %al, %dx" : : "{al}"(val), "{dx}"(port));
 }
 
 #[inline(always)]
+#[allow(dead_code)]
 pub unsafe fn outw(port: u16, val: u16) {
     asm!("outw %ax, %dx" : : "{ax}"(val), "{dx}"(port));
 }
 
 #[inline(always)]
+#[allow(dead_code)]
 pub unsafe fn outl(port: u16, val: u32) {
     asm!("outl %eax, %dx" : : "{eax}"(val), "{dx}"(port));
 }
